aaae99659f56e8d8ab81ada849055ba0d26b5338
QX_Tao <pyt7836500@163.com>
2023-04-14 12:28:16 +0800

    Commit.

--- ceshijilibrary/src/main/java/com/tencent/automationlib/Automation.java
+++ ceshijilibrary/src/main/java/com/tencent/automationlib/Automation.java
@@ -1,15 +1,27 @@
 package com.tencent.automationlib;
 
+import android.app.Application;
+
 public class Automation {
     public static void enable(int configCode){
         SdkServer sdkServer = new SdkServer(9000);
-        Thread thread = new Thread(() -> {
-            try {
-                sdkServer.startListen();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-        });
+
+        Thread thread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            sdkServer.startListen();
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
         thread.start();
+    }
+
+    public static void setupApplication(Application app) {
+
     }
 }
